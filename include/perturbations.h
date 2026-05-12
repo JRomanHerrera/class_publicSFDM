97373877b3838c04f3fcd80660381f568c081ac0
JRomanHerrera#exitgit config --global user.name JRomanHerreracd ..make cleanecho <jo.romanherrera@ugto.mx>
2026-05-11 18:32:07 -0600

    Mod for perturbation.h

--- include/perturbations.h
+++ include/perturbations.h
@@ -247,6 +247,7 @@ struct perturbations
   short has_source_delta_idr;  /**< do we need source for delta of interacting dark radiation? */
   short has_source_delta_dcdm; /**< do we need source for delta of DCDM? */
   short has_source_delta_fld;  /**< do we need source for delta of dark energy? */
+  short has_source_delta_sfdm;  /**< do we need source for delta from scalar field dark matter? */
   short has_source_delta_scf;  /**< do we need source for delta from scalar field? */
   short has_source_delta_dr;   /**< do we need source for delta of decay radiation? */
   short has_source_delta_ur;   /**< do we need source for delta of ultra-relativistic neutrinos/relics? */
@@ -261,6 +262,7 @@ struct perturbations
   short has_source_theta_idr;  /**< do we need source for theta of interacting dark radiation? */
   short has_source_theta_dcdm; /**< do we need source for theta of DCDM? */
   short has_source_theta_fld;  /**< do we need source for theta of dark energy? */
+  short has_source_theta_sfdm;  /**< do we need source for theta of scalar field dark matter? */
   short has_source_theta_scf;  /**< do we need source for theta of scalar field? */
   short has_source_theta_dr;   /**< do we need source for theta of ultra-relativistic neutrinos/relics? */
   short has_source_theta_ur;   /**< do we need source for theta of ultra-relativistic neutrinos/relics? */
@@ -294,6 +296,9 @@ struct perturbations
   int index_tp_delta_idm; /**< index value for delta of interacting dark matter */
   int index_tp_delta_dcdm;/**< index value for delta of DCDM */
   int index_tp_delta_fld;  /**< index value for delta of dark energy */
+  int index_tp_delta_sfdm;  /**< index value for delta of scalar field dark matter */ 
+  int index_tp_delta0_sfdm;  /**< index value for delta auxiliary of scalar field dark matter */ 
+  int index_tp_delta1_sfdm;  /**< index value for delta auxiliary of scalar field dark matter */ 
   int index_tp_delta_scf;  /**< index value for delta of scalar field */
   int index_tp_delta_dr; /**< index value for delta of decay radiation */
   int index_tp_delta_ur; /**< index value for delta of ultra-relativistic neutrinos/relics */
@@ -310,6 +315,7 @@ struct perturbations
   int index_tp_theta_cdm;   /**< index value for theta of cold dark matter */
   int index_tp_theta_dcdm;  /**< index value for theta of DCDM */
   int index_tp_theta_fld;   /**< index value for theta of dark energy */
+  int index_tp_theta_sfdm;  /**< index value for theta of scalar field dark matter */
   int index_tp_theta_scf;   /**< index value for theta of scalar field */
   int index_tp_theta_ur;    /**< index value for theta of ultra-relativistic neutrinos/relics */
   int index_tp_theta_idr;   /**< index value for theta of interacting dark radiation */
@@ -480,6 +486,8 @@ struct perturbations_vector
   int index_pt_delta_fld;  /**< dark energy density in true fluid case */
   int index_pt_theta_fld;  /**< dark energy velocity in true fluid case */
   int index_pt_Gamma_fld;  /**< unique dark energy dynamical variable in PPF case */
+  int index_pt_delta0_sfdm;  /**< scalar field dark matter auxiliary density */
+  int index_pt_delta1_sfdm;  /**< scalar field dark matter auxiliary density  */
   int index_pt_phi_scf;  /**< scalar field density */
   int index_pt_phi_prime_scf;  /**< scalar field velocity */
   int index_pt_delta_ur; /**< density of ultra-relativistic neutrinos/relics */
